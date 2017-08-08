2d654e9dcfd9dc42e5c73c539ffdcc1b30ae0059
Alex (Jinghao) Yan <yanhao1991@gmail.com>
2017-08-08 16:08:38 +0800

    extract base typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,17 +1,1 @@
-declare const require: (path: string) => any
-
-// used for import json files (webpack support json file import)
-declare module "*.json" {
-  const value: any
-  export default value
-}
-
-// for react hot reload
-declare var module: { hot: any }
-
-// work around to avoid compile error when using antd >= 2.12.3
-declare module "rc-util/lib/Dom/addEventListener" {
-  export type RcUtilEventHandler = any
-}
-
-declare const ENV_PREFIX: any
+/// <reference path="../node_modules/es-base/types/index.d.ts" />
